b3de888a59d988083c815636b5b1c13b2a6ac4b1
Samiullah Shaikh <samiullah21january@gmail.com>
2025-11-05 17:33:17 +0500

    Some key chnages

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,21 +1,53 @@
 export default [
-    'strapi::errors',
+  'strapi::errors',
   {
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['http://localhost:3000'], // your Next.js frontend
+      origin: [
+        'http://localhost:3000', // for local dev
+        'https://karachi-water-sewerage-frontend-9g5.vercel.app', // production frontend
+      ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeadersOnError: true,
     },
   },
   'strapi::logger',
-  'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+
+  // 👇 Add this middleware at the very end
+  async (ctx, next) => {
+    // Force Strapi to recognize Railway’s HTTPS proxy
+    ctx.request.header['x-forwarded-proto'] = 'https';
+    await next();
+  },
 ];
+
+// export default [
+//     'strapi::errors',
+//   {
+//     name: 'strapi::cors',
+//     config: {
+//       enabled: true,
+//       origin: ['http://localhost:3000'], // your Next.js frontend
+//       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     },
+//   },
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
