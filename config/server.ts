4caea808d05c1790cca4fdcacfd713ae2e2a1dcd
Samiullah Shaikh <samiullah21january@gmail.com>
2025-11-05 17:14:50 +0500

    Changes in server

--- config/server.ts
+++ config/server.ts
@@ -1,10 +1,3 @@
-// export default ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
-// });
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
@@ -14,9 +7,38 @@ export default ({ env }) => ({
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'default_jwt_secret'),
-      options: {
-        secure: false, // 👈 Add this line
-      },
+    },
+    // 👇 Disable secure cookies and force non-https mode for Railway
+    url: '/admin',
+    serveAdminPanel: true,
+    cookies: {
+      secure: false,
     },
   },
+  url: env('PUBLIC_URL', 'http://0.0.0.0:1337'),
 });
+
+
+// export default ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   admin: {
+//     auth: {
+//       secret: env('ADMIN_JWT_SECRET', 'default_jwt_secret'),
+//       options: {
+//         secure: false, // 👈 Add this line
+//       },
+//     },
+//   },
+// });
+
+// export default ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
