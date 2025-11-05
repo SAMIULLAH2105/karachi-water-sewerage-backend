1c9931e9fe87d62163481fc006c0f462046a5eac
Samiullah Shaikh <samiullah21january@gmail.com>
2025-11-05 17:37:49 +0500

    Some key chnages

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -5,30 +5,23 @@ export default [
     config: {
       enabled: true,
       origin: [
-        'http://localhost:3000', // for local dev
-        'https://karachi-water-sewerage-frontend-9g5.vercel.app', // production frontend
+        'http://localhost:3000',
+        'https://karachi-water-sewerage-frontend-9g5.vercel.app'
       ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      keepHeadersOnError: true,
+      headers: '*',
     },
   },
-  'strapi::logger',
   'strapi::security',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-
-  // 👇 Add this middleware at the very end
-  async (ctx, next) => {
-    // Force Strapi to recognize Railway’s HTTPS proxy
-    ctx.request.header['x-forwarded-proto'] = 'https';
-    await next();
-  },
 ];
+
 
 // export default [
 //     'strapi::errors',
