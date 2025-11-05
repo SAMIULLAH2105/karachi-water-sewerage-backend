0fd7aee37062f5836ac7357f23e409a86b382954
Samiullah Shaikh <samiullah21january@gmail.com>
2025-11-05 17:42:45 +0500

    Some key chnages

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,46 +1,21 @@
 export default [
-  'strapi::errors',
+    'strapi::errors',
   {
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: [
-        'http://localhost:3000',
-        'https://karachi-water-sewerage-frontend-9g5.vercel.app'
-      ],
+      origin: ['http://localhost:3000'], // your Next.js frontend
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      headers: '*',
     },
   },
-  'strapi::security',
-  'strapi::poweredBy',
   'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
-
-
-// export default [
-//     'strapi::errors',
-//   {
-//     name: 'strapi::cors',
-//     config: {
-//       enabled: true,
-//       origin: ['http://localhost:3000'], // your Next.js frontend
-//       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//     },
-//   },
-//   'strapi::logger',
-//   'strapi::errors',
-//   'strapi::security',
-//   'strapi::cors',
-//   'strapi::poweredBy',
-//   'strapi::query',
-//   'strapi::body',
-//   'strapi::session',
-//   'strapi::favicon',
-//   'strapi::public',
-// ];
