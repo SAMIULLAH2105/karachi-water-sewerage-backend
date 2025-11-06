9893f16acf15119a3196d3c940aa9996fb1a72cc
Samiullah Shaikh <samiullah21january@gmail.com>
2025-11-06 16:33:15 +0500

    Cleared Cache and Package lock for fresh build

--- config/server.ts
+++ config/server.ts
@@ -20,26 +20,44 @@ export default ({ env }) => ({
 
 
 
+// // export default ({ env }) => ({
+// //   host: env('HOST', '0.0.0.0'),
+// //   port: env.int('PORT', 1337),
+// //   app: {
+// //     keys: env.array('APP_KEYS'),
+// //   },
+// //   admin: {
+// //     auth: {
+// //       secret: env('ADMIN_JWT_SECRET', 'default_jwt_secret'),
+// //       options: {
+// //         secure: false, // 👈 Add this line
+// //       },
+// //     },
+// //   },
+// // });
+
+// // export default ({ env }) => ({
+// //   host: env('HOST', '0.0.0.0'),
+// //   port: env.int('PORT', 1337),
+// //   app: {
+// //     keys: env.array('APP_KEYS'),
+// //   },
+// // });
+
 // export default ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
+//   host: env('HOST', '127.0.0.1'),
 //   port: env.int('PORT', 1337),
+//   url: env('PUBLIC_URL', 'http://localhost:1337'),
 //   app: {
-//     keys: env.array('APP_KEYS'),
+//     keys: env.array('APP_KEYS', ['localKey1', 'localKey2']),
 //   },
 //   admin: {
 //     auth: {
-//       secret: env('ADMIN_JWT_SECRET', 'default_jwt_secret'),
-//       options: {
-//         secure: false, // 👈 Add this line
-//       },
+//       secret: env('ADMIN_JWT_SECRET', 'localSecretKey'),
+//     },
+//     cookies: {
+//       secure: false, // local should be non-secure
 //     },
 //   },
-// });
-
-// export default ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
+//   proxy: false, // ❌ no proxy for local
 // });
